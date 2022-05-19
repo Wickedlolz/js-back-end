36554cd8b9b84b7e2abac51b6ce793e64d6288a9
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2022-05-19 17:53:12 +0300

    add home handler

--- cat-shelter/handlers/home.js
+++ cat-shelter/handlers/home.js
@@ -1,2 +1,29 @@
-const fs = require('fs/promises');
+const fs = require('fs');
 const path = require('path');
+const cats = require('../data/cats.json');
+
+module.exports = (req, res) => {
+    const pathname = new URL(req.url, 'http://localhost:3000').pathname;
+    let filePath = path.normalize(
+        path.join(__dirname, '../views/home/index.html')
+    );
+
+    if (pathname == '/' && req.method == 'GET') {
+        fs.readFile(filePath, (error, data) => {
+            if (error) {
+                console.error(error);
+                res.writeHead(404, {
+                    'Content-Type': 'text/plain',
+                });
+
+                res.write('404 Not Found');
+                res.end();
+            } else {
+                res.write(data);
+                res.end();
+            }
+        });
+    } else {
+        return true;
+    }
+};
--- cat-shelter/index.js
+++ cat-shelter/index.js
@@ -1,7 +1,14 @@
 const http = require('http');
 const config = require('./config/config');
+const handlers = require('./handlers');
 
-const server = http.createServer((req, res) => {});
+const server = http.createServer((req, res) => {
+    for (let handler of handlers) {
+        if (!handler(req, res)) {
+            break;
+        }
+    }
+});
 
 server.listen(config.PORT, () =>
     console.log('Server are listening on PORT: ' + config.PORT)
