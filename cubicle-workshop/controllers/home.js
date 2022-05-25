bad8dd0919a8bbbe0dc300115182ac86c7673987
Viktor Dimitrov <52072434+Wickedlolz@users.noreply.github.com>
2022-05-25 17:03:20 +0300

    add search

--- cubicle-workshop/controllers/home.js
+++ cubicle-workshop/controllers/home.js
@@ -2,11 +2,32 @@ const router = require('express').Router();
 const cubeService = require('../services/cube');
 
 router.get('/', async (req, res) => {
-    const cubes = await cubeService.getAll();
+    let cubes = await cubeService.getAll();
+
+    if (req.query.search) {
+        cubes = cubes.filter((c) =>
+            c.name
+                .toLocaleLowerCase()
+                .includes(req.query.search.toLocaleLowerCase())
+        );
+    }
+
+    if (req.query.from) {
+        cubes = cubes.filter(
+            (c) => c.difficultyLevel >= Number(req.query.from)
+        );
+    }
+
+    if (req.query.to) {
+        cubes = cubes.filter((c) => c.difficultyLevel <= Number(req.query.to));
+    }
 
     res.locals = {
         title: 'Browse',
         cubes,
+        searchedValue: req.query.search,
+        from: req.query.from,
+        to: req.query.to,
     };
 
     res.render('index');
