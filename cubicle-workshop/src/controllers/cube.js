0f4b35e1e7dfcf0c4058756fc838edeb7b64bbe1
Wickedlolz <iceshotgg.linux@gmail.com>
2022-06-12 19:01:19 +0300

    add create, edit cube validation and error handling

--- cubicle-workshop/src/controllers/cube.js
+++ cubicle-workshop/src/controllers/cube.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 
 const cubeService = require('../services/cube');
 const accessoryService = require('../services/accessory');
+const { body, validationResult } = require('express-validator');
+const { mapErrors } = require('../utils/mapErrors');
 
 const { isUser, isCreator } = require('../middlewares/guards');
 
@@ -9,23 +11,67 @@ router.get('/create', isUser(), (req, res) => {
     res.render('create', { title: 'Create Cube' });
 });
 
-router.post('/create', isUser(), async (req, res) => {
-    const data = {
-        name: req.body.name.trim(),
-        description: req.body.description.trim(),
-        imageUrl: req.body.imageUrl.trim(),
-        difficultyLevel: req.body.difficultyLevel,
-        creatorId: res.locals.user.id,
-    };
+router.post(
+    '/create',
+    isUser(),
+    body('name').trim(),
+    body('description').trim(),
+    body('imageUrl').trim(),
+    body('name')
+        .notEmpty()
+        .withMessage('Cube Name is required!')
+        .bail()
+        .isLength({ min: 5 })
+        .withMessage('Cube name must be atleast 5 characters long.')
+        .bail()
+        .isAlphanumeric('en-US', { ignore: ' ' })
+        .withMessage(
+            'Cube name could be English letters, digits, and whitespaces.'
+        ),
+    body('description')
+        .notEmpty()
+        .withMessage('Description is required!')
+        .bail()
+        .isLength({ min: 20 })
+        .withMessage('Description must be atlease 20 characters long.')
+        .bail()
+        .isAlphanumeric('en-US', { ignore: ' ' })
+        .withMessage(
+            'Description could be English letters, digits, and whitespaces.'
+        ),
+    body('imageUrl')
+        .notEmpty()
+        .withMessage('Image URL is required!')
+        .bail()
+        .custom(
+            (value) => value.startsWith('http') || value.startsWith('https')
+        )
+        .withMessage(
+            'Referring to actual picture (starts with http://... or https://...)'
+        ),
+    async (req, res) => {
+        const { errors } = validationResult(req);
+        const data = {
+            name: req.body.name.trim(),
+            description: req.body.description.trim(),
+            imageUrl: req.body.imageUrl.trim(),
+            difficultyLevel: req.body.difficultyLevel,
+            creatorId: res.locals.user.id,
+        };
 
-    try {
-        const cube = await cubeService.create(data);
-        res.redirect('/details/' + cube._id);
-    } catch (error) {
-        Object.values(error.errors).forEach((e) => console.log(e.message));
-        res.render('404');
+        try {
+            if (errors.length > 0) {
+                throw errors;
+            }
+
+            const cube = await cubeService.create(data);
+            res.redirect('/details/' + cube._id);
+        } catch (error) {
+            const errors = mapErrors(error);
+            res.render('create', { errors, data });
+        }
     }
-});
+);
 
 router.get('/details/:id', async (req, res) => {
     const id = req.params.id;
@@ -89,24 +135,69 @@ router.get('/edit/:id', isUser(), isCreator(), async (req, res) => {
     res.render('editCubePage', { title: `Edit - ${cube.name}`, cube });
 });
 
-router.post('/edit/:id', isUser(), isCreator(), async (req, res) => {
-    const cubeId = req.params.id;
+router.post(
+    '/edit/:id',
+    isUser(),
+    isCreator(),
+    body('name').trim(),
+    body('description').trim(),
+    body('imageUrl').trim(),
+    body('name')
+        .notEmpty()
+        .withMessage('Cube Name is required!')
+        .bail()
+        .isLength({ min: 5 })
+        .withMessage('Cube name must be atleast 5 characters long.')
+        .bail()
+        .isAlphanumeric('en-US', { ignore: ' ' })
+        .withMessage(
+            'Cube name could be English letters, digits, and whitespaces.'
+        ),
+    body('description')
+        .notEmpty()
+        .withMessage('Description is required!')
+        .bail()
+        .isLength({ min: 20 })
+        .withMessage('Description must be atlease 20 characters long.')
+        .bail()
+        .isAlphanumeric('en-US', { ignore: ' ' })
+        .withMessage(
+            'Description could be English letters, digits, and whitespaces.'
+        ),
+    body('imageUrl')
+        .notEmpty()
+        .withMessage('Image URL is required!')
+        .bail()
+        .custom(
+            (value) => value.startsWith('http') || value.startsWith('https')
+        )
+        .withMessage(
+            'Referring to actual picture (starts with http://... or https://...)'
+        ),
+    async (req, res) => {
+        const { errors } = validationResult(req);
+        const cubeId = req.params.id;
 
-    const data = {
-        name: req.body.name.trim(),
-        description: req.body.description.trim(),
-        imageUrl: req.body.imageUrl.trim(),
-        difficultyLevel: req.body.difficultyLevel,
-    };
+        const data = {
+            name: req.body.name.trim(),
+            description: req.body.description.trim(),
+            imageUrl: req.body.imageUrl.trim(),
+            difficultyLevel: req.body.difficultyLevel,
+        };
 
-    try {
-        const cube = await cubeService.update(cubeId, data);
-        res.redirect('/details/' + cube._id);
-    } catch (error) {
-        console.error(error);
-        res.render('404');
+        try {
+            if (errors.length > 0) {
+                throw errors;
+            }
+
+            const cube = await cubeService.update(cubeId, data);
+            res.redirect('/details/' + cube._id);
+        } catch (error) {
+            const errors = mapErrors(error);
+            res.render('editCubePage', { errors, cube: data });
+        }
     }
-});
+);
 
 router.get('/delete/:id', isUser(), isCreator(), async (req, res) => {
     const cubeId = req.params.id;
