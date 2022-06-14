45ae31dc0294a602410efa2c05bf62d85d3cfb33
Wickedlolz <iceshotgg.linux@gmail.com>
2022-06-14 06:46:59 +0300

    fix login view

--- exam-prep-1/controllers/user.js
+++ exam-prep-1/controllers/user.js
@@ -73,7 +73,7 @@ router.post(
     isGuest(),
     body('username').trim(),
     body('password').trim(),
-    body('usernamem')
+    body('username')
         .notEmpty()
         .withMessage('Username is required!')
         .bail()
