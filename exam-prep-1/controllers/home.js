ca55cf4a6ca3ee67faf736befd17444f795f0eab
Wickedlolz <iceshotgg.linux@gmail.com>
2022-06-14 12:40:00 +0300

    add show all publications with share count

--- exam-prep-1/controllers/home.js
+++ exam-prep-1/controllers/home.js
@@ -1,7 +1,18 @@
 const router = require('express').Router();
 
-router.get('/', (req, res) => {
-    res.render('home');
+const galleryService = require('../services/gallery');
+
+router.get('/', async (req, res) => {
+    const publications = await galleryService
+        .getAll()
+        .populate('usersShared')
+        .lean();
+
+    publications.map((p) => (p.shareCount = p.usersShared.length));
+
+    console.log(publications);
+
+    res.render('home', { publications });
 });
 
 module.exports = router;
