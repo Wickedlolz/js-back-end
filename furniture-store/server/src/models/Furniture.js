d8232c17b928ad9c8dce7363cdc3b131ca7ae2ba
Wickedlolz <iceshotgg.linux@gmail.com>
2022-06-24 16:24:09 +0300

    add furniture model

--- furniture-store/server/src/models/Furniture.js
+++ furniture-store/server/src/models/Furniture.js
@@ -1,5 +1,19 @@
-const { Schema, model } = require('mongoose');
+const {
+    Schema,
+    model,
+    Types: { ObjectId },
+} = require('mongoose');
 
-const furnitureSchema = new Schema({});
+const furnitureSchema = new Schema({
+    make: { type: String, required: true },
+    model: { type: String, required: true },
+    year: { type: Number, required: true },
+    description: { type: String, required: true },
+    price: { type: Number, required: true },
+    material: { type: String },
+    _ownerId: { type: ObjectId, ref: 'User' },
+});
 
 const Furniture = model('Furniture', furnitureSchema);
+
+module.exports = Furniture;
