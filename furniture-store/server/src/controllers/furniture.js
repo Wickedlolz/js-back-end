807bfd3cbed3262930c76d6f6bf2aee14e150462
Wickedlolz <iceshotgg.linux@gmail.com>
2022-06-24 17:13:06 +0300

    add isAuth  guard

--- furniture-store/server/src/controllers/furniture.js
+++ furniture-store/server/src/controllers/furniture.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 
+const { isAuth } = require('../middlewares/guards');
 const furnitureService = require('../services/furniture');
 
 router.get('/', async (req, res) => {
@@ -11,6 +12,17 @@ router.get('/', async (req, res) => {
     }
 });
 
-router.post('/', async (req, res) => {});
+router.post('/', isAuth(), async (req, res) => {
+    const item = {
+        make: req.body.make,
+        model: req.body.model,
+        year: req.body.year,
+        description: req.body.description,
+        price: req.body.price,
+        img: req.body.img,
+        material: req.body.material,
+        _ownerId: req.user._id,
+    };
+});
 
 module.exports = router;
---
+++ furniture-store/server/src/middlewares/guards.js
@@ -0,0 +1,9 @@
+exports.isAuth = function () {
+    return (req, res, next) => {
+        if (req.user) {
+            next();
+        } else {
+            res.status(401).json({ message: 'Please log in' });
+        }
+    };
+};
