120c5524ce61bf0c32d4ab8869d35b5f84703919
Wickedlolz <iceshotgg.linux@gmail.com>
2022-06-24 16:34:42 +0300

    add user model

---
+++ furniture-store/server/src/models/User.js
@@ -0,0 +1,20 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema({
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+});
+
+userSchema.index(
+    { email: 1 },
+    {
+        collation: {
+            locale: 'en',
+            strength: 2,
+        },
+    }
+);
+
+const User = model('User', userSchema);
+
+module.exports = User;
